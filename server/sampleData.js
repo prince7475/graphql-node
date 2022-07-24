b7b126e31235a18e66aea3fda01c78c5c2fed470
Prince Donkor <prince7475>
2022-07-23 19:46:11 -0500

    inital push

---
+++ server/sampleData.js
@@ -0,0 +1,59 @@
+const projects = [{
+    id: '1',
+    clientId: '1',
+    name: 'Project 1',
+    description: 'description 1',
+    status: 'In Progress',
+},
+{
+    id: '2',
+    clientId: '2',
+    name: 'Project 2',
+    description: 'description 2',
+    status: 'In Progress',
+},
+{
+    id: '3',
+    clientId: '3',
+    name: 'Project 3',
+    description: 'description 3',
+    status: 'In Progress',
+},
+{
+    id: '4',
+    clientId: '4',
+    name: 'Project 4',
+    description: 'description 4',
+    status: 'In Progress',
+}
+]
+
+
+const clients = [{
+    id: '1',
+    name: 'Client 1',
+    email: 'email1@gmail.com',
+    phone: '123-456-7899',
+},
+{
+    id: '2',
+    name: 'Client 2',
+    email: 'email2@gmail.com',
+    phone: '223-456-7899',
+},
+{
+    id: '3',
+    name: 'Client 3',
+    email: 'email3@gmail.com',
+    phone: '323-456-7899',
+},
+{
+    id: '4',
+    name: 'Client 4',
+    email: 'email4@gmail.com',
+    phone: '423-456-7899',
+}
+]
+
+
+module.exports = {clients,projects}
